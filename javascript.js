51eb9ffc149df89dd6d7b0770308cad41ff4dc1f
Alan Davila <alandavila@Alans-MacBook-Pro.local>
2018-10-03 14:36:50 -0500

    removed some animals from hard coded list

--- javascript.js
+++ javascript.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 
     var animals = [
-      "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
-      "bird", "ferret", "turtle", "sugar glider", "chinchilla",
-      "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
-      "capybara", "teacup pig", "serval", "salamander", "frog"
+      "dog", "cat", "rabbit", "hamster", "skunk", "bird", "turtle", "goat", "chicken", "frog"
     ];
   
     // function to make buttons and add to page
